3dbbc33ba71f149cb9bd80b406b9db9d6ef8b709
Eric Shen <eric@collectiveequity.com>
2024-09-09 19:13:07 +0300

    Created team page and worked on home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,15 +1,64 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
-import "../assets/font-size.css"
 import "./styles/home.css"
 
 
 function Home() {
 	return (
-		<Container fluid id="main-content">
-            <Container className="vision-statement">
-                    <h4>We create <i>personal financial stability,</i></h4>
+	<Container fluid id="main-content">
+            <Container fluid className="vision-statement">
+                    <h4>We <i>create liquidity and diversify risk</i></h4>
                     <h4>so you can focus on building what matters.</h4>
+            </Container>
+            <Container className="success-stories">
+                <div>
+                        <h4>Success Stories Placeholder</h4>
+                        <p>We need stories of successful founders, such as Rebel Energy, with a portrait of the Founder and some quote endorsing Collective Equity</p>
+                </div>
+            </Container>
+            <Container fluid className="next-fund">
+                <div className="next-fund-title">
+                        <h4>
+                                Get involved in our $500m flagship fund
+                        </h4>
+                </div>
+                
+                <div className="next-fund-body">
+                        <div className="founders-segment">
+                                <h4>Calling all Founders (and Co-founders) who:</h4>
+                                <ul>
+                                        <li>
+                                                Run a US or UK headquartered private business
+                                        </li>
+                                        <li>
+                                                Which has just raised funding since March 2023
+                                        </li>
+                                        <li>
+                                                Owns a $10m minimum stake in the business
+                                        </li>
+                                        <li>
+                                                Would like to participate in the success of other similar businesses
+                                        </li>
+                                </ul>
+                        </div>
+                        <div className="non-active-segment">
+                                <h4>Calling all shareholders & ex-employees who:</h4>
+                                <ul>
+                                        <li>
+                                                Own shares in a US or UK headquartered private business
+                                        </li>
+                                        <li>
+                                                Which has just raised funding since March 2023
+                                        </li>
+                                        <li>
+                                                Owns a $2m minimum stake in the business
+                                        </li>
+                                        <li>
+                                                Is interested in liquidating or diversifying their stake into other similar investments
+                                        </li>
+                                </ul>
+                        </div>
+                </div>
             </Container>
         </Container>
 	);
--- src/routes/Team.tsx
+++ src/routes/Team.tsx
@@ -7,13 +7,19 @@ function Team() {
 		<Container fluid className="team-page">
 			<Container fluid className="team-intro">
 				<p className="team-title">Meet the Collective</p>
-				<div>
+				<div className="team-intro-body">
 					<h3 className="team-text">At the core of what we do, we are a community of innovators and risk takers. Our firm was born in a London cafe in 2017, when our founding partners reflected on how crazy it was for founders to <i>risk losing 100% of their net worth</i> for a plethora of potential reasons. We spent the next 7 years transforming the UK funding ecosystem through the creation of pools of similar, high growth start-ups.</h3>
 					<br/>
 					<h4 className="team-text-smaller">We see the human side of being a founder, and while we're obsessed with offering the best tool for founders to diversify their exposure, we care immensely about the businesses and people we partner with. We foster a collaborative and close-knit community, not just within our team but also with all of our founder partners.</h4>
+					<br/>
+					<br/>
+					<img className="team-img" src="src/assets/Team.jpg"/>
+					<p className="team-img-caption">Mike Royston, Brian Pallas & Tristan Schnegg, Founding Partners at Collective Equity </p>
 				</div>	
+				
 			</Container>
-			<Container fluid className="team-cards-investment">
+
+			<Container fluid className="team-cards-title">
 
 						Investment
 			</Container>
@@ -23,6 +29,11 @@ function Team() {
 					<Col>
 						<Card className="team-card">
 							<Card.Img className="team-card-img" variant="top" src="src/assets/Brian.jpg" />
+							<a>
+								<div className="mask hide">
+									Read full bio
+								</div>
+							</a>
 							<Card.Body className="team-card-body">
 								<Card.Title className="team-card-name">Brian Pallas</Card.Title>
 								<Card.Text className="team-card-position">
@@ -54,9 +65,124 @@ function Team() {
 						</Card>
 					</Col>
 				</Row>
+				<br/>
+				<Row>
+					<Col>
+						<Card className="team-card">
+							<Card.Img className="team-card-img" variant="top" src="src/assets/Eric.jpeg" />
+							<Card.Body className="team-card-body">
+								<Card.Title className="team-card-name">Eric Shen</Card.Title>
+								<Card.Text className="team-card-position">
+									Investment Analyst
+								</Card.Text>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col>
+						<Card className="team-card">
+							<Card.Img className="team-card-img" variant="top" src="src/assets/Ziyi.jpeg" />
+							<Card.Body className="team-card-body">
+								<Card.Title className="team-card-name">Ziyi Yap</Card.Title>
+								<Card.Text className="team-card-position">
+									Investment Analyst
+								</Card.Text>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col>
+					</Col>
+				</Row>
 			</Container>
+
+			<Container fluid className="team-cards-title">
+
+						Investment Committee
+			</Container>
+
+			<Container fluid="lg" className="team-cards">		
+				<Row>
+					<Col>
+						<Card className="team-card">
+							<Card.Img className="team-card-img" variant="top" src="src/assets/Ornit.jpeg" />
+							<Card.Body className="team-card-body">
+								<Card.Title className="team-card-name">Ornit Shinar</Card.Title>
+								<Card.Text className="team-card-position">
+									Investment Committee
+								</Card.Text>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col>
+						<Card className="team-card">
+							<Card.Img className="team-card-img" variant="top" src="src/assets/Harel.jpeg" />
+							<Card.Body className="team-card-body">
+								<Card.Title className="team-card-name">Harel Kodesh</Card.Title>
+								<Card.Text className="team-card-position">
+									Investment Committee
+								</Card.Text>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col>
+						<Card className="team-card">
+							<Card.Img className="team-card-img" variant="top" src="src/assets/Sanjin.jpeg" />
+							<Card.Body className="team-card-body">
+								<Card.Title className="team-card-name">Sanjin Beloberk</Card.Title>
+								<Card.Text className="team-card-position">
+									Investment Committee
+								</Card.Text>
+							</Card.Body>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+
+			<Container fluid className="team-cards-title">
+
+						Advisors
+			</Container>
+
+			<Container fluid="lg" className="team-cards">		
+				<Row>
+					<Col>
+						<Card className="team-card">
+							<Card.Img className="team-card-img" variant="top" src="src/assets/David.jpeg" />
+							<Card.Body className="team-card-body">
+								<Card.Title className="team-card-name">David Carr</Card.Title>
+								<Card.Text className="team-card-position">
+									Compliance Officer
+								</Card.Text>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col>
+						<Card className="team-card">
+							<Card.Img className="team-card-img" variant="top" src="src/assets/Charles.jpg" />
+							<Card.Body className="team-card-body">
+								<Card.Title className="team-card-name">Charles Nahum</Card.Title>
+								<Card.Text className="team-card-position">
+									Advisor
+								</Card.Text>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col>
+						<Card className="team-card">
+							<Card.Img className="team-card-img" variant="top" src="src/assets/Guy.jpeg" />
+							<Card.Body className="team-card-body">
+								<Card.Title className="team-card-name">Guy Rigby</Card.Title>
+								<Card.Text className="team-card-position">
+									Advisor
+								</Card.Text>
+							</Card.Body>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+			<br/>
+			<br/>
+			<br/>
 		</Container>
-		
 	);
 }
 
